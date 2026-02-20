ed6f59c96b94ad1b22c7d71300d140471bbd0b4a
Edoardo Zoni <ezoni@lbl.gov>
2026-02-20 11:16:45 -0800

    Improve upstream example validation

--- tests/validate_upstream_examples.py
+++ tests/validate_upstream_examples.py
@@ -1,6 +1,11 @@
 import argparse
 
 from pals import load
+from pals.kinds import PlaceholderName
+from pals.kinds.BeamLine import BeamLine
+from pals.kinds.Drift import Drift
+from pals.kinds.Lattice import Lattice
+from pals.kinds.Quadrupole import Quadrupole
 
 
 def main():
@@ -15,7 +20,17 @@ def main():
     example_file = args.path
     # Parse and validate YAML data from file
     lattice = load(example_file)
-    print(lattice.facility[0])
+    assert isinstance(lattice.facility[0], Drift)
+    assert lattice.facility[0].name == "drift1"
+    assert isinstance(lattice.facility[1], Quadrupole)
+    assert lattice.facility[1].name == "quad1"
+    assert isinstance(lattice.facility[2], BeamLine)
+    assert lattice.facility[2].name == "fodo_cell"
+    assert isinstance(lattice.facility[3], BeamLine)
+    assert lattice.facility[3].name == "fodo_channel"
+    assert isinstance(lattice.facility[4], Lattice)
+    assert lattice.facility[4].name == "fodo_lattice"
+    assert isinstance(lattice.facility[5], PlaceholderName)
 
 
 if __name__ == "__main__":
